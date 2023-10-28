24170e341fbdadccf2375e25692d06208309f591
css <cauefc12@gmail.com>
2023-10-28 15:49:50 -0300

    SETTING MYSQL DATASOURCE

--- src/main/java/com/estudo/estudoapi/di/notificacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.estudo.estudoapi.di.notificacao;
-
-import com.estudo.estudoapi.di.modelo.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensagem);
-
-}
