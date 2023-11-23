f3f86085b525974b3399c83997c78f208a096580
css <cauefc12@gmail.com>
2023-11-23 10:28:42 -0300

    LOMBOK ADDED IN PROJECT

--- src/main/java/com/estudo/estudoapi/domain/model/Cozinha.java
+++ src/main/java/com/estudo/estudoapi/domain/model/Cozinha.java
@@ -1,56 +1,22 @@
 package com.estudo.estudoapi.domain.model;
 
-import java.util.Objects;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	// @Column(name = "nom_cozinha")
-	// quando nao queremos mudar o nome da coluna, não é necessário anotar usando @Column
+
 	private String nome;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		return Objects.equals(id, other.id);
-	}
-	
 }
