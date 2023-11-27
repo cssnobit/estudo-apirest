265e5bd3e1b7f6ed5f2edfc9e5cc2c12873541c1
css <cauefc12@gmail.com>
2023-11-27 13:33:25 -0300

    IMPLEMENTATION ESTADO REPOSITORY

--- src/main/java/com/estudo/estudoapi/infrastructure/repository/EstadoRepositoryImpl.java
+++ src/main/java/com/estudo/estudoapi/infrastructure/repository/EstadoRepositoryImpl.java
@@ -9,34 +9,35 @@ import com.estudo.estudoapi.domain.repository.EstadoRepository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
 @Component
 public class EstadoRepositoryImpl implements EstadoRepository {
 
 	@PersistenceContext
-	private EntityManager entity;
+	private EntityManager manager;
 	
 	@Override
 	public List<Estado> todas() {
-		// TODO Auto-generated method stub
-		return entity.createQuery("from Estado", Estado.class).getResultList();
+		return manager.createQuery("from Estado", Estado.class).getResultList();
 	}
 
 	@Override
 	public Estado porId(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return manager.find(Estado.class, id);
 	}
 
+	@Transactional
 	@Override
 	public Estado adicionar(Estado estado) {
-		// TODO Auto-generated method stub
-		return null;
+		return manager.merge(estado);
 	}
 
+	@Transactional
 	@Override
 	public void remover(Estado estado) {
-		// TODO Auto-generated method stub
+		estado = porId(estado.getId());
+		manager.remove(estado);
 		
 	}
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,13 +5,14 @@ insert into forma_pagamento (descricao) values ('Pagamento feito no PIX');
 insert into forma_pagamento (descricao) values ('Pagamento feito no dinheiro');
 insert into forma_pagamento (descricao) values ('Pagamento feito no cartao de credito');
 
-insert into restaurante (nome, taxa_frete, cozinha_id, forma_pagamento_id) values ('Thaitai', 10.0, 1, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id, forma_pagamento_id) values ('Tandoor', 12.0, 2, 3);
-insert into restaurante (nome, taxa_frete, cozinha_id, forma_pagamento_id) values ('Reet Namaste', 11.50, 2, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id, forma_pagamento_id) values ('Thaitai Restaurante', 10.0, 1, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id, forma_pagamento_id) values ('Tandoor Indian Food', 12.0, 2, 3);
+insert into restaurante (nome, taxa_frete, cozinha_id, forma_pagamento_id) values ('Reet Namaste Brasil', 11.50, 2, 2);
 
 
-insert into permissao (nome, descricao) values ('Ver prato', 'Permite consultar a descricao do prato');
-insert into permissao (nome, descricao) values ('Ver cozinha', 'Permite consultar o tipo de cozinha do restaurante');
+insert into permissao (nome, descricao) values ('VER_PRATO', 'Permite consultar a descricao do prato');
+insert into permissao (nome, descricao) values ('VER_COZINHA', 'Permite consultar o tipo de cozinha do restaurante');
+insert into permissao (nome, descricao) values ('ADICIONAR_COZINHA', 'Permite adicionar um tipo de cozinha do restaurante');
 
 insert into estado (nome) values ('Sao Paulo');
 insert into estado (nome) values ('Minas Gerais');
